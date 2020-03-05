b593501d784f1a0453e00e14301fd973b299db45
Tim Foster <tim.foster@joyent.com>
2020-03-05 12:05:57 +0000

    Placeholder configure and setup scripts

--- boot/setup.sh
+++ boot/setup.sh
@@ -4,7 +4,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Copyright 2019 Joyent, Inc.
+# Copyright 2020 Joyent, Inc.
 #
 
 #
@@ -24,34 +24,23 @@ set -o pipefail
 set -o xtrace
 
 PATH=/opt/local/bin:/opt/local/sbin:/usr/bin:/usr/sbin
+ROOT_DIR=/opt/triton/minio
 
-NODE=/opt/triton/prometheus/build/node/bin/node
-
-ROOT_DIR=/opt/triton/prometheus
-
-#
-# named-related paths. Keep in sync with "bin/prometheus-configure".
-#
-NAMED_DIR=${ROOT_DIR}/named
-NAMED_LOG_DIR=/var/log/named
-
-#
-# CMON key-related paths. Keep in sync with "bin/certgen" and
-# "bin/prometheus-configure".
 #
 CMON_AUTH_DIR=${ROOT_DIR}/keys
 CMON_KEY_FILE=${CMON_AUTH_DIR}/prometheus.key.pem
 CMON_CERT_FILE=${CMON_AUTH_DIR}/prometheus.cert.pem
 
 #
-# Prometheus data that should be persistent across reprovisions is stored on its
+# Minio data that should be persistent across reprovisions is stored on its
 # delegate dataset:
 #
-#   /data/prometheus/
-#       data/    # time-series database
+#   /data/minio/
 #
-PERSIST_DIR=/data/prometheus
+PERSIST_DIR=/data/minio
 DATA_DIR=${PERSIST_DIR}/data
+
+mkdir -p /data/minio
 
 function fatal {
     printf '%s: ERROR: %s\n' "$(basename ${0})" "${*}" >&2
@@ -73,7 +62,7 @@ fi
 # ---- internal routines
 
 # Mount our delegate dataset at /data.
-function prometheus_setup_delegate_dataset {
+function minio_setup_delegate_dataset {
     local mountpoint
 
     dataset=zones/$(zonename)/data
@@ -83,121 +72,28 @@ function prometheus_setup_delegate_dataset {
     fi
 }
 
-function prometheus_setup_env {
-    if [[ ! $(grep prometheus /root/.profile) ]]; then
-        echo '' >>/root/.profile
-        echo "export PATH=${ROOT_DIR}/bin:${ROOT_DIR}/prometheus:\$PATH" \
-            >>/root/.profile
-    fi
-}
-
-function prometheus_setup_named {
-    local localhost_zone
-    local arpa_zone
-
-    svccfg import /opt/local/lib/svc/manifest/bind.xml
-
-    mkdir -p "${NAMED_DIR}"
-    mkdir -p "${NAMED_DIR}/master"
-    mkdir -p "${NAMED_DIR}/slave"
-    mkdir -p "${NAMED_LOG_DIR}"
-
-    read -rd '' localhost_zone <<LOCALHOST_ZONE || true
-\$TTL 3D
-
-\$ORIGIN localhost.
-
-@       1D      IN     SOA     @       root (
-                       0               ; serial
-                       8H              ; refresh
-                       2H              ; retry
-                       4W              ; expiry
-                       1D              ; minimum
-                       )
-
-@       IN      NS      @
-        IN      A       127.0.0.1
-LOCALHOST_ZONE
-
-    read -rd '' arpa_zone <<ARPA_ZONE || true
-\$TTL 3D
-
-@       IN      SOA     localhost. root.localhost. (
-                        0               ; Serial
-                        8H              ; Refresh
-                        2H              ; Retry
-                        4W              ; Expire
-                        1D              ; Minimum TTL
-                        )
-
-       IN      NS      localhost.
-
-1      IN      PTR     localhost.
-ARPA_ZONE
-
-    echo -e "${localhost_zone}" > "${NAMED_DIR}/master/localhost"
-    echo -e "${arpa_zone}" > "${NAMED_DIR}/master/127.in-addr.arpa"
-
-    chown -R named:named \
-        "${NAMED_DIR}" \
-        "${NAMED_LOG_DIR}"
-}
-
-function prometheus_setup_prometheus {
+function minio_setup_minio {
     mkdir -p "${DATA_DIR}"
 
-    /usr/sbin/svccfg import ${ROOT_DIR}/smf/manifests/prometheus.xml
+    /usr/sbin/svccfg import ${ROOT_DIR}/smf/manifests/minio.xml
 
     #
-    # Set up key and client certificate used to auth with this DC's CMON.
-    #
-    echo 'Generating key and client cert for CMON auth'
-    mkdir -p "${CMON_AUTH_DIR}"
-    "${NODE}" '--abort_on_uncaught_exception' \
-        ${ROOT_DIR}/bin/certgen "${FLAVOR}"
-
-    #
-    # The prometheus SMF service runs as the 'nobody' user, so the files it
+    # The minio SMF service runs as the 'nobody' user, so the files it
     # accesses must be owned by nobody. Here, we ensure this for the files and
     # directory that will remain static for the lifetime of the zone.
     #
-    chown nobody:nobody \
-        "${CMON_KEY_FILE}" \
-        "${CMON_CERT_FILE}" \
-        "${DATA_DIR}" \
+    chown nobody:nobody "${DATA_DIR}"
 
     #
     # prometheus-configure contains the common setup code that must be run here
     # and also on config-agent updates
     #
-    TRACE=1 ${ROOT_DIR}/bin/prometheus-configure
+    TRACE=1 ${ROOT_DIR}/bin/minio-configure
 }
-
-function prometheus_initialize_global_zones_json {
-    # Start with an empty file
-    echo '[]' > ${ROOT_DIR}/etc/global_zones.json
-}
-
-function prometheus_setup_crontab {
-    # Setup crontab
-    tmp_crontab=/tmp/prometheus-$$.cron
-    minute=$((RANDOM % 60))
-    crontab -l > ${tmp_crontab}
-    [[ $? -eq 0 ]] || fatal "Unable to write to ${tmp_crontab}"
-    echo '' >>${tmp_crontab}
-    echo '# update the global_zones.json file' >>${tmp_crontab}
-    # BASHSTYLED
-    echo "${minute} * * * * ${ROOT_DIR}/bin/update_global_zones.sh >>/var/log/update_global_zones.log 2>&1" >>${tmp_crontab}
-    crontab ${tmp_crontab}
-    [[ $? -eq 0 ]] || fatal 'Unable to import crontab'
-    rm -f ${tmp_crontab}
-}
-
 
 # ---- mainline
 
-prometheus_setup_delegate_dataset
-prometheus_setup_env
+minio_setup_delegate_dataset
 
 if [[ "${FLAVOR}" == 'manta' ]]; then
 
@@ -207,10 +103,9 @@ if [[ "${FLAVOR}" == 'manta' ]]; then
 
     manta_common_presetup
     manta_add_manifest_dir "${ROOT_DIR}"
-    manta_common_setup 'prometheus' 0
+    manta_common_setup 'minio' 0
 
-    prometheus_setup_named
-    prometheus_setup_prometheus
+    minio_setup_minio
 
     manta_common_setup_end
 
@@ -220,15 +115,12 @@ else # "$FLAVOR" == "triton"
     source /opt/smartdc/boot/lib/util.sh
     sdc_common_setup
 
-    prometheus_initialize_global_zones_json
-    prometheus_setup_crontab
-    prometheus_setup_named
-    prometheus_setup_prometheus
+    minio_setup_minio
 
     # Log rotation.
     sdc_log_rotation_add config-agent /var/svc/log/*config-agent*.log 1g
     sdc_log_rotation_add registrar /var/svc/log/*registrar*.log 1g
-    sdc_log_rotation_add prometheus /var/svc/log/*prometheus*.log 1g
+    sdc_log_rotation_add prometheus /var/svc/log/*minio*.log 1g
     sdc_log_rotation_setup_end
 
     #
